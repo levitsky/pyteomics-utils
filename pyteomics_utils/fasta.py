cfdb2d7a03abb3a594252b54f05b020a1864f75f
Lev Levitsky <lev.levitsky@phystech.edu>
2020-03-21 17:23:12 +0300

    Add prefix and decoy_only args

--- pyteomics_utils/fasta.py
+++ pyteomics_utils/fasta.py
@@ -5,7 +5,8 @@ logger = logging.getLogger(__name__)
 
 def decoy(args):
     logger.debug('decoy called with: %s', args)
-    fasta.write_decoy_db(mode=args.mode, keep_nterm=args.keep_nterm, keep_cterm=args.keep_cterm)
+    fasta.write_decoy_db(mode=args.mode, keep_nterm=args.keep_nterm, keep_cterm=args.keep_cterm,
+        prefix=args.prefix, decoy_only=args.decoy_only)
 
 
 def register_commands(subparsers, parents=[]):
@@ -14,5 +15,7 @@ def register_commands(subparsers, parents=[]):
     fasta_decoy = fasta_commands.add_parser('decoy', parents=parents)
     fasta_decoy.set_defaults(func=decoy)
     fasta_decoy.add_argument('-m', '--mode', choices=fasta._decoy_functions.keys(), default='reverse')
-    fasta_decoy.add_argument('--keep-nterm', action='store_true')
-    fasta_decoy.add_argument('--keep-cterm', action='store_true')
+    fasta_decoy.add_argument('--keep-nterm', action='store_true', help='Keep N-terminal residue in decoy sequences')
+    fasta_decoy.add_argument('--keep-cterm', action='store_true', help='Keep C-terminal residue in decoy sequences')
+    fasta_decoy.add_argument('-p', '--prefix', default='DECOY_', help='Decoy prefix')
+    fasta_decoy.add_argument('--decoy-only', action='store_true', help='Do not include original sequences in output')

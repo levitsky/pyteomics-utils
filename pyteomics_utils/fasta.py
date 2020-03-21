120a64c59fc6f909105ebdc4799914dd4c323917
Lev Levitsky <lev.levitsky@phystech.edu>
2020-03-21 17:17:09 +0300

    Add logging, add first working command

--- pyteomics_utils/fasta.py
+++ pyteomics_utils/fasta.py
@@ -1,10 +1,18 @@
+import logging
 from pyteomics import fasta
+logger = logging.getLogger(__name__)
+
 
 def decoy(args):
-    print('decoy called with:', args)
+    logger.debug('decoy called with: %s', args)
+    fasta.write_decoy_db(mode=args.mode, keep_nterm=args.keep_nterm, keep_cterm=args.keep_cterm)
 
-def register_commands(subparsers):
-    fasta_parser = subparsers.add_parser('fasta')
+
+def register_commands(subparsers, parents=[]):
+    fasta_parser = subparsers.add_parser('fasta', parents=parents)
     fasta_commands = fasta_parser.add_subparsers()
-    fasta_decoy = fasta_commands.add_parser('decoy')
+    fasta_decoy = fasta_commands.add_parser('decoy', parents=parents)
     fasta_decoy.set_defaults(func=decoy)
+    fasta_decoy.add_argument('-m', '--mode', choices=fasta._decoy_functions.keys(), default='reverse')
+    fasta_decoy.add_argument('--keep-nterm', action='store_true')
+    fasta_decoy.add_argument('--keep-cterm', action='store_true')
--- pyteomics_utils/main.py
+++ pyteomics_utils/main.py
@@ -1,11 +1,22 @@
 import argparse
+import logging
 from . import fasta
 
+
 def main():
-    main_parser = argparse.ArgumentParser()
+    common_parser = argparse.ArgumentParser(add_help=False)
+    common_parser.add_argument('-v', '--verbosity', choices=range(3), type=int, default=1,
+        help='Output verbosity')
+    main_parser = argparse.ArgumentParser(parents=[common_parser])
+    levels = [logging.WARNING, logging.INFO, logging.DEBUG]
 
     modules = main_parser.add_subparsers()
-    fasta.register_commands(modules)
+    fasta.register_commands(modules, [common_parser])
 
     args = main_parser.parse_args()
+
+    logging.basicConfig(format='{levelname:>8}: {asctime} {message}',
+                        datefmt='[%H:%M:%S]', level=levels[args.verbosity], style='{')
+    logger = logging.getLogger(__name__)
+    logger.debug('Received args in main: %s', args)
     args.func(args)
